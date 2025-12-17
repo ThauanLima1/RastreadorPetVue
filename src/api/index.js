f3931e6e2ae64fde77fba930f052c18dc96dc278
Cassiano <henriquecassiano1@gmail.com>
2025-12-16 21:59:08 -0300

    Final

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://ec2-100-26-48-34.compute-1.amazonaws.com:8080/api/';
+const BASE_URL = 'http://localhost:8080/api/';
 
 export async function get(endpoint) {
   console.log('Buscando em:', `${BASE_URL}${endpoint}`);
